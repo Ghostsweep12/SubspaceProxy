6a0ab2add58484a9802bf025a22ec2a655abbc15
ghostsweep12 <ghostsweep12@gmail.com>
2025-12-19 12:55:31 -0500

    Incorrect installation fixed New wayland incompatibility workaround added

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,31 @@
+use tauri::Manager;
+use std::time::Duration;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+
+        .setup(|app| {
+            if let Some(window) = app.get_webview_window("main") {
+                tauri::async_runtime::spawn(async move {
+                    // Wait for the GPU
+                    tokio::time::sleep(Duration::from_millis(10)).await;
+                    // Reload
+                    let _ = window.eval("window.location.reload()");
+                });
+            }
+            Ok(())
+        })
+
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
